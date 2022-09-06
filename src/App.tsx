b2916991fdbc122eb9a73d7dc98217ebd167e204
Tyler Gray <tylergrayx@gmail.com>
2022-09-05 21:03:43 -0400

    just empty react app

--- src/App.tsx
+++ src/App.tsx
@@ -11,37 +11,3 @@ function App() {
 export default App;
 
 
-
-
-
-
-class Square {
-  constructor(public width: number) {};
-};
-
-class Rectangle extends Square {
-  constructor(public width: number, public height: number) {
-    super(width);
-  };
-};
-
-type Shape = Square | Rectangle;
-
-function calculateArea(shape: Shape) {
-  if (shape instanceof Rectangle) {
-    return shape.height * shape.width;
-  } else {
-    return shape.width * shape.width;
-  };
-};
-
-let square = new Square(5);
-let rectangle = new Rectangle(10, 6);
-
-console.log(calculateArea(rectangle));
-console.log(calculateArea(square));
-
-rectangle.height = 1.5;
-
-console.log(calculateArea(rectangle));
-
